d4040d0dfcf1373d0b4b9fb17fcdac1d8866ba62
MysteriousMagenta <therealrebuilder@gmail.com>
2014-12-03 22:29:41 +0100

    Made kittens better, made it importable.

--- extra_kittens/kittens.py
+++ extra_kittens/kittens.py
@@ -2,11 +2,27 @@ import urllib.request
 import random
 import string
 
-width = random.randrange(100, 1000)
-height = random.randrange(100, 1000)
-url = "http://www.placekitten.com/g/" + str(width) + "/" + str(height)
-image = urllib.request.urlopen(url).read()
-filename = ""
-for item in range(random.randint(10, 25)): filename += random.choice(string.ascii_letters)
-with open(filename + ".png", "wb") as kitten:
-    kitten.write(image)
+def genSize():
+    width = random.randrange(100, 1000)
+    height = random.randrange(100, 1000)
+    return width, height
+
+def getImage()
+    width, height = genSize()
+    url = "http://www.placekitten.com/g/" + str(width) + "/" + str(height)
+    img = urllib.request.urlopen(url).read()
+    return img
+
+def genTitle():
+    filename = ""
+    for item in range(5):
+        filename += random.choice(string.ascii_letters)
+        
+def main()
+    filename = genTitle()
+    image = genImage()
+    with open(filename + ".png", "wb") as kitten:
+        kitten.write(image)
+
+if __name__ == "__main__":
+    main()
